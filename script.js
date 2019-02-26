4bfe85a189a03645f4e7f31644e1f4287cdb411c
LeonardoFrederiche <leofrederiche@gmail.com>
2019-02-25 23:50:50 -0300

    Refactoring

--- script.js
+++ script.js
@@ -2,37 +2,32 @@ $('document').ready(function(){
 
 	const confetti 	= "<div class='confetti'></div>";
 	var hexColors 	= ['#e74c3c', '#e67e22', '#9b59b6', '#3498db', '#2ecc71', '#ecf0f1'];
-	var screenWidth	= $(window).width() - 20;
 
-	function RandomColor(){
+	function RandomDownColor(){
 		return Math.floor((Math.random() * 6))
 	}
 
-	function GenerateConfetti(){
-		var color 		= hexColors[RandomColor()];
-		var left 		= Math.floor((Math.random() * screenWidth));
-		var rotation	= [0,0,0,0];
+	function RandomLeft(){
+		var screenWidth	= $(window).width() - 20;
 
-		rotation[0] 	= Math.floor((Math.random() * 2));
-		rotation[1]		= Math.floor((Math.random() * 2));
-		rotation[2]		= Math.floor((Math.random() * 2));
-		rotation[3]		= Math.floor((Math.random() * 1000));
+		return Math.floor((Math.random() * screenWidth));
+	}
+
+	function GenerateConfetti(){
+		var color 	= hexColors[RandomDownColor()];
 
 		$(confetti).appendTo('body').css({
 			'background' : color,
-			'left': left
+			'left': RandomLeft()
 
 		}).addClass(
-			'down' + RandomColor()
+			'down' + RandomDownColor()
 		);
 	};
 
 	$('.btnSuccess').click(function(){
-		var count = 0;
-
 		setInterval(function(){
 			GenerateConfetti();
-			count++;
 		}, 100);
 	});
 
