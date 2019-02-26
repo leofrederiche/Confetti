69e2010bf33fedd0069914021877a55407ac6384
LeonardoFrederiche <leofrederiche@gmail.com>
2019-02-26 00:19:10 -0300

    Organization and Limit to time Dopamine

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 $('document').ready(function(){
 
 	const confetti 	= "<div class='confetti'></div>";
-	var hexColors 	= ['#e74c3c', '#e67e22', '#9b59b6', '#3498db', '#2ecc71', '#ecf0f1'];
 
-	function RandomDownColor(){
+	function RandomStyle(){
 		return Math.floor((Math.random() * 6))
 	}
 
 	function RandomLeft(){
 		var screenWidth	= $(window).width() - 20;
-
 		return Math.floor((Math.random() * screenWidth));
 	}
 
 	function GenerateConfetti(){
-		var color 	= hexColors[RandomDownColor()];
-
 		$(confetti).appendTo('body').css({
-			'background' : color,
 			'left': RandomLeft()
-
 		}).addClass(
-			'down' + RandomDownColor()
+			'style' + RandomStyle()
 		);
 	};
 
 	$('.btnSuccess').click(function(){
+		var count = 0;
+
 		setInterval(function(){
-			GenerateConfetti();
-		}, 100);
+			if (count < 50){
+				GenerateConfetti();	
+				count++;
+			}
+
+			console.log(count);
+		}, 50);
 	});
 
 });
